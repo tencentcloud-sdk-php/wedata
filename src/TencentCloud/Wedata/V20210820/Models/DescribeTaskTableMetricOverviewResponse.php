6c087db56260a309f011e23f6c7f985be592a59e
tencentcloudapi <tencentcloudapi@tencent.com>
2025-01-17 05:35:09 +0800

    release 3.0.1304

--- src/TencentCloud/Wedata/V20210820/Models/DescribeTaskTableMetricOverviewResponse.php
+++ src/TencentCloud/Wedata/V20210820/Models/DescribeTaskTableMetricOverviewResponse.php
@@ -18,42 +18,36 @@ namespace TencentCloud\Wedata\V20210820\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * CreateOfflineTask返回参数结构体
+ * DescribeTaskTableMetricOverview返回参数结构体
  *
- * @method string getTaskId() 获取任务ID
+ * @method array getTaskTableMetricInfos() 获取表粒度指标集合
+ * @method void setTaskTableMetricInfos(array $TaskTableMetricInfos) 设置表粒度指标集合
+ * @method integer getTotalCount() 获取总数
+ * @method void setTotalCount(integer $TotalCount) 设置总数
+ * @method string getMetricType() 获取返回列表类型
 注意：此字段可能返回 null，表示取不到有效值。
- * @method void setTaskId(string $TaskId) 设置任务ID
-注意：此字段可能返回 null，表示取不到有效值。
- * @method string getArrangeSpaceTaskId() 获取导入到编排空间的任务id
-注意：此字段可能返回 null，表示取不到有效值。
- * @method void setArrangeSpaceTaskId(string $ArrangeSpaceTaskId) 设置导入到编排空间的任务id
-注意：此字段可能返回 null，表示取不到有效值。
- * @method string getData() 获取结果
-注意：此字段可能返回 null，表示取不到有效值。
- * @method void setData(string $Data) 设置结果
+ * @method void setMetricType(string $MetricType) 设置返回列表类型
 注意：此字段可能返回 null，表示取不到有效值。
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class CreateOfflineTaskResponse extends AbstractModel
+class DescribeTaskTableMetricOverviewResponse extends AbstractModel
 {
     /**
-     * @var string 任务ID
-注意：此字段可能返回 null，表示取不到有效值。
+     * @var array 表粒度指标集合
      */
-    public $TaskId;
+    public $TaskTableMetricInfos;
 
     /**
-     * @var string 导入到编排空间的任务id
-注意：此字段可能返回 null，表示取不到有效值。
+     * @var integer 总数
      */
-    public $ArrangeSpaceTaskId;
+    public $TotalCount;
 
     /**
-     * @var string 结果
+     * @var string 返回列表类型
 注意：此字段可能返回 null，表示取不到有效值。
      */
-    public $Data;
+    public $MetricType;
 
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
@@ -61,11 +55,9 @@ class CreateOfflineTaskResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param string $TaskId 任务ID
-注意：此字段可能返回 null，表示取不到有效值。
-     * @param string $ArrangeSpaceTaskId 导入到编排空间的任务id
-注意：此字段可能返回 null，表示取不到有效值。
-     * @param string $Data 结果
+     * @param array $TaskTableMetricInfos 表粒度指标集合
+     * @param integer $TotalCount 总数
+     * @param string $MetricType 返回列表类型
 注意：此字段可能返回 null，表示取不到有效值。
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
@@ -82,16 +74,21 @@ class CreateOfflineTaskResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("TaskId",$param) and $param["TaskId"] !== null) {
-            $this->TaskId = $param["TaskId"];
+        if (array_key_exists("TaskTableMetricInfos",$param) and $param["TaskTableMetricInfos"] !== null) {
+            $this->TaskTableMetricInfos = [];
+            foreach ($param["TaskTableMetricInfos"] as $key => $value){
+                $obj = new TaskTableMetricInfo();
+                $obj->deserialize($value);
+                array_push($this->TaskTableMetricInfos, $obj);
+            }
         }
 
-        if (array_key_exists("ArrangeSpaceTaskId",$param) and $param["ArrangeSpaceTaskId"] !== null) {
-            $this->ArrangeSpaceTaskId = $param["ArrangeSpaceTaskId"];
+        if (array_key_exists("TotalCount",$param) and $param["TotalCount"] !== null) {
+            $this->TotalCount = $param["TotalCount"];
         }
 
-        if (array_key_exists("Data",$param) and $param["Data"] !== null) {
-            $this->Data = $param["Data"];
+        if (array_key_exists("MetricType",$param) and $param["MetricType"] !== null) {
+            $this->MetricType = $param["MetricType"];
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
